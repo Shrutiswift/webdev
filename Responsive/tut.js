b598a443afe833681fb5e4f2ecf81371c63db25f
Shruti <shrutiswift@gmail.com>
2022-06-24 12:42:33 +0530

    functions

--- Responsive/tut.js
+++ Responsive/tut.js
@@ -1,41 +1,13 @@
-// console.log("good night");
-// let element = document.getElementById('me');
-// element.style.color = 'red';
-// console.log(element);
-  // var str = 'Shruti';
-    // var newString = str.concat(' New String')
-    // console.log(newString)
-    // var strWithWhitespaces = " This contains a string ";
-    // console.log(strWithWhitespaces)
-    // console.log(strWithWhitespaces.trim())
-    // var char2 = str.charAt(2);
-    // console.log(char2)
-    // function gfg(){
-    //     document.getElementById("me").innerHTML = "Geeks For Geeks";
-    // }
-    // let string1 = "Hey!";
-    // let string2 = "This is a string";
-    // console.log(string2);
-    // const a = "This cannot be changed";
-    // console.log(a);
-    // let age = 2;
-    // if(age > 18){
-    //     console.log("You can drink cold drink");
-    // }
-    // else{
-    //     console.log("You can drink water");
-    // }
-    // const cups = 45;
-    // switch (cups){
-    //     case 4:
-    //         console.log("hayeee")
-    //         break;
-    //     case 18:
-    //         console.log("qwerty")
-    //     default:
-    //         console.log("default value")
-    //         break;
-    // }
+function input(name){
+    console.log(name + ' is 18yrs old');
+}
+let name = "Shruti";
+let name2 = "Abhi";
+let name3 = "Raju";
+input(name);
+input(name2);
+input(name3);
+
 
 
 
