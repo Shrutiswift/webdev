2c791e80c4c3dbd3c58ce3be599f6df2fc541eab
Shruti <shrutiswift@gmail.com>
2022-06-24 18:05:14 +0530

    html

--- Responsive/tut.js
+++ Responsive/tut.js
@@ -1,18 +1,19 @@
-function input(name){
-    console.log(name + ' is 18yrs old');
-}
-let name = "Shruti";
-let name2 = "Abhi";
-let name3 = "Raju";
-input(name);
-input(name2);
-input(name3);
-
-
-
-
-
-
+// function input(name){
+//     console.log(name + ' is 18yrs old');
+// }
+// let name = "Shruti";
+// let name2 = "Abhi";
+// let name3 = "Raju";
+// input(name);
+// input(name2);
+// input(name3);
+// alert("This is a message");
+// alert("https://api.opsgenie.com/v2/alerts/requests/:requestId");
+navigator.geolocation.getCurrentPosition(function(position) {
+    alert('allow');
+}, function() {
+    alert('deny'? allow:void)
+});
 
 
 
