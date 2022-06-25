08cb1fca4b3b571f9adf5f764a9f1b0911681a96
Shruti <shrutiswift@gmail.com>
2022-06-25 14:18:09 +0530

    prompt

--- Responsive/tut.js
+++ Responsive/tut.js
@@ -9,12 +9,31 @@
 // input(name3);
 // alert("This is a message");
 // alert("https://api.opsgenie.com/v2/alerts/requests/:requestId");
-navigator.geolocation.getCurrentPosition(function(position) {
-    alert('allow');
-}, function() {
-    alert('deny'? allow:void)
-});
+// navigator.geolocation.getCurrentPosition(function(position) {
+//     window.alert('allow');
+// }, function() {
+//     window.alert('deny');
+// });
+// function doExpensiveWork() {
+//     var done = false;
+    // ...
+    // this part of the function takes up to five milliseconds
+    // set done to true if we're done
+    // ...
+//     return done;
+// }
 
+// function rescheduleWork() {
+//     var id = setTimeout(rescheduleWork, 0); // preschedule next iteration
+//     if (doExpensiveWork())
+//         clearTimeout(id); // clear the timeout if we don't need it
+// }
+// function scheduleWork() {
+//     setTimeout(rescheduleWork, 0);
+// }
+// scheduleWork();
+let name = prompt("What is your name?" , "Guest");
+console.log(name);
 
 
 
